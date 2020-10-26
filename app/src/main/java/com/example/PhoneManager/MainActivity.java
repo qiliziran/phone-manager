7e4a681a7a27a1fce0cc0d7b5814311b0093abd8
liulhong <1944785902@qq.com>
2020-10-26 21:14:11 +0800

    启用后台获取用户数据功能

--- app/src/main/java/com/example/PhoneManager/MainActivity.java
+++ app/src/main/java/com/example/PhoneManager/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.PhoneManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 
+import com.example.PhoneManager.DataBase.UserData;
+import com.example.PhoneManager.Service.GetUserDataService;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,10 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import org.litepal.LitePal;
+
+import pl.com.salsoft.sqlitestudioremote.SQLiteStudioService;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +25,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LitePal.getDatabase();
+        //启动 后台获取用户app数据 服务
+        Intent StartIntent = new Intent(MainActivity.this, GetUserDataService.class);
+        startService(StartIntent);
+
+        //开启SQLiteStudio数据库调试服务
+        SQLiteStudioService.instance().start(this);
+
+        //Check if permission enabled
+        if (GetUserDataService.getUsageStatsList(this).isEmpty()){
+            Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+            startActivity(intent);
+        }
+
 //        this.getSupportActionBar().hide();
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
